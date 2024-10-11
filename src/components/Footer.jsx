4dfe778bb666bc1f90caca95763ecc13cd93be61
Mathias N <65249743+matnvd@users.noreply.github.com>
2024-10-11 14:16:45 -0400

    stupid commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Footer = (props) => {
+  const bgStyle = { backgroundColor: "#f5f5f5" };
+
+  return (
+    <footer style={bgStyle} className="mt-auto py-5 text-center ">
+      <Container>
+        {props.children}
+        {/* <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
+        <a
+          rel="noopener"
+          href="https://github.com/hashirshoaeb"
+          aria-label="My GitHub"
+        > <span className="badge bg-dark">
+            Hashir Shoaib
+          </span>
+        </a>{" "}
+        using <i className="fab fa-react" /> */}
+        <p>
+          <small className="text-muted">
+            Project code is open source and drew inspiration from Hashir Shoaeb. Feel free to email me at
+          </small>
+          <a className="text-decoration-none" href="mailto:mathiasnvd07@gmail.com"> mathiasnvd07@gmail.com</a>.
+        </p>
+        
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
